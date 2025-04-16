2ad71a582f80daf55485e752845fc83fe2c951cd
Raphael Cochez <raphael_cochez@hotmail.com>
2025-04-16 12:31:21 +0200

    Added email.java and user.java models, these are not completed and might be adjusted in the near future.
    Added database_test.md to test/java/server/service general database test docs

--- src/model/Email.java
+++ src/model/Email.java
@@ -1,5 +1,81 @@
 package model;
 
 public class Email {
+    private String id;
+    private String to;
+    private String from;
+    private String subject;
+    private String body;
+    private String timestamp;
+    private boolean visible;
+    private boolean edited;
 
+    public Email() {
+    } // Default constructor required for Gson
+
+    // Optionally add constructor, getters, setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public boolean isEdited() {
+        return edited;
+    }
+
+    public void setEdited(boolean edited) {
+        this.edited = edited;
+    }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,30 @@
 package model;
 
 public class User {
+    private String email;
+    private String hashedPassword;
 
+    public User() {
+    } // Default constructor required for Gson
+
+    public User(String email, String hashedPassword) {
+        this.email = email;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
 }
