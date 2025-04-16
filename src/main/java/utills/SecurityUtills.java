f1f3b75e2b609b364c06223ce919b9eb56ae1cb4
Raphael Cochez <raphael_cochez@hotmail.com>
2025-04-16 16:35:37 +0200

    Delete - duplicate file AuthService.java - file utills
    Update
    - src\test\java\server\service\SessionManagerTest.md - file naming
    
    Added
    - src\main\java\utills\SecurityUtills.java
    - src\test\java\utills\SecurityUtillsTest.java
    - src\test\java\utills\SecurityUtillsTest.md

---
+++ src/main/java/utills/SecurityUtills.java
@@ -0,0 +1,33 @@
+// inspiration from https://stackoverflow.com/questions/69791042/how-to-validate-user-password-after-hashing-using-sha-256-salted-in-java
+
+package utills;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class SecurityUtills {
+
+    public static String hashPassword(String password, String salt) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(Constants.HASH_ALGORITHM);
+            digest.update(salt.getBytes());
+            byte[] hashedBytes = digest.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hashedBytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password", e);
+        }
+    }
+
+    public static String generateSalt() {
+        byte[] salt = new byte[Constants.SALT_LENGTH];
+        new SecureRandom().nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+
+    public static boolean verifyPassword(String plainPassword, String storedHashedPassword, String salt) {
+        String hashedInput = hashPassword(plainPassword, salt);
+        return hashedInput.equals(storedHashedPassword);
+    }
+}
